f1699ba7a0abd89402747043d01ebfa99b9ffc73
samane Tohidi <s.tohidi39@gmail.com>
2024-06-13 19:14:44 +0330

    Improvement ui

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../models/detail_view_model.dart';
 import '../repository/detail_view_request.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class DetailScreen extends StatefulWidget {
   final int id;
@@ -41,7 +43,7 @@ class _DetailScreenState extends State<DetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text(
+        title: Text(
           _detailViewModel?.collection?.title ?? 'Detail',
           style: const TextStyle(fontSize: 14.0),
         ),
@@ -50,60 +52,73 @@ class _DetailScreenState extends State<DetailScreen> {
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
           : _errorMessage != null
-              ? Center(child: Text('Error: $_errorMessage'))
-              : _detailViewModel != null
-                  ? Column(
-                      children: [
-                        Expanded(
-                          child: Image.network(
-                            _detailViewModel?.collection?.image ?? '',
-                            fit: BoxFit.contain,
-                            errorBuilder: (context, error, stackTrace) =>
-                                const Icon(Icons.error),
-                            loadingBuilder: (context, child, loadingProgress) {
-                              if (loadingProgress == null) return child;
-                              return const Center(
-                                  child: CircularProgressIndicator());
-                            },
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            children: [
-                               Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text(_detailViewModel?.collection?.description ?? '',
-                                  style: const TextStyle(),
-                                ),
-                              ),
-                              const SizedBox(height: 10.0),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  ElevatedButton.icon(
-                                    onPressed: () {
-                                      // Add your share functionality here
-                                    },
-                                    icon: const Icon(Icons.share),
-                                    label: const Text('Share'),
-                                  ),
-                                  ElevatedButton.icon(
-                                    onPressed: () {
-                                      // Add your save functionality here
-                                    },
-                                    icon: const Icon(Icons.save),
-                                    label: const Text('Save'),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    )
-                  : const Center(child: Text('No details available')),
+          ? Center(child: Text('Error: $_errorMessage'))
+          : _detailViewModel != null
+          ? Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 80.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  FractionallySizedBox(
+                    widthFactor: 1.0,
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.6,
+                      child: Image.network(
+                        _detailViewModel?.collection?.image ?? '',
+                        fit: BoxFit.contain,
+                        errorBuilder: (context, error, stackTrace) =>
+                        const Icon(Icons.error),
+                        loadingBuilder: (context, child, loadingProgress) {
+                          if (loadingProgress == null) return child;
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      _detailViewModel?.collection?.description ?? '',
+                      style: const TextStyle(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: Colors.white,
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // Add your share functionality here
+                    },
+                    icon: const Icon(Icons.share),
+                    label: const Text('Share'),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // Add your save functionality here
+                    },
+                    icon: const Icon(Icons.save),
+                    label: const Text('Save'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      )
+          : const Center(child: Text('No details available')),
     );
   }
 }
